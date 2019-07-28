c5a5a763d511d96ee0ad59ae40ba83b620b59b9c
Adri- <adrien.boitelle@gmail.com>
2019-07-28 14:49:32 +0200

    strings optimized

--- Pulsarc/UI/Screens/Gameplay/UI/TextDisplayElementFixedSize.cs
+++ Pulsarc/UI/Screens/Gameplay/UI/TextDisplayElementFixedSize.cs
@@ -7,19 +7,50 @@ namespace Pulsarc.UI.Screens.Gameplay.UI
 {
     class TextDisplayElementFixedSize : TextDisplayElement
     {
-        public dynamic lastValue;
+        public string lastValueString;
+        public int lastValueInt;
+        public long lastValueLong;
+        public double lastValueDouble;
+
         public string append;
         public TextDisplayElementFixedSize(string name, Vector2 position, string append, int fontSize = 18, bool centered = false) : base (name,position, fontSize, centered)
         {
             this.append = append;
         }
 
-        public void Update(dynamic value)
+        public new void Update(string value)
         {
-            if (value != lastValue)
+            if (value != lastValueString)
             {
-                base.Update((string) (value.ToString())+append);
-                lastValue = value;
+                base.Update(value.ToString() + append);
+                lastValueString = value;
+            }
+        }
+
+        public void Update(int value)
+        {
+            if (value != lastValueInt)
+            {
+                base.Update(value.ToString() + append);
+                lastValueInt = value;
+            }
+        }
+
+        public void Update(long value)
+        {
+            if (value != lastValueLong)
+            {
+                base.Update(value.ToString() + append);
+                lastValueLong = value;
+            }
+        }
+
+        public void Update(double value)
+        {
+            if (value != lastValueDouble)
+            {
+                base.Update(value.ToString() + append);
+                lastValueDouble = value;
             }
         }
     }
