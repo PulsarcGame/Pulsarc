1ba8f215d958823798d96e6b580394d63ed1fae6
Adri- <adrien.boitelle@gmail.com>
2019-09-02 17:04:43 +0200

    add SQLite databases and Score.db

---
+++ Pulsarc/Utils/SQLite/SQLiteStore.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SQLite;
+
+namespace Pulsarc.Utils.SQLite
+{
+    public abstract class SQLiteStore
+    {
+        SQLiteConnection db;
+        string name;
+
+        public SQLiteStore(string name_)
+        {
+            name = name_;
+
+            if (!File.Exists(name)) {
+                SQLiteConnection.CreateFile(name+".db");
+                connect();
+                initDB();
+            } else
+            {
+                connect();
+            }
+
+
+        }
+
+        public void connect()
+        {
+            db = new SQLiteConnection("Data Source="+name+".db;Version=3;");
+            db.Open();
+        }
+
+        public void exec(string sql)
+        {
+            new SQLiteCommand(sql, db).ExecuteNonQuery();
+        }
+
+        public SQLiteDataReader query(string sql)
+        {
+            return new SQLiteCommand(sql, db).ExecuteReader();
+        }
+
+        public List<object> queryFirst(string sql)
+        {
+            List<object> res = new List<object>();
+            SQLiteDataReader reader = query(sql);
+            reader.Read();
+            res.Add(reader);
+            return res;
+        }
+
+        public void close()
+        {
+            db.Close();
+        }
+
+        public abstract void initDB();
+    }
+}
---
+++ Pulsarc/Utils/SQLite/ScoreDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pulsarc.Utils.SQLite
+{
+    public class ScoreDB : SQLiteStore
+    {
+        public ScoreDB() : base("scores") { }
+        public override void initDB()
+        {
+            exec("CREATE TABLE score (map bigint, score int, grade char, max mediumint, perfect mediumint, great mediumint, good mediumint, bad mediumint, miss mediumint)");
+            exec("CREATE TABLE replay (map bigint, replaydata BLOB, float performance)");
+        }
+
+        public void addScore(int map, int score, char grade, int max, int perfect, int great, int good, int bad, int miss)
+        {
+            exec("INSERT INTO score (map, score, grade, max, perfect, great, good, bad, miss) VALUES " +
+                "( " + map + "," + score + ",'" + grade + "'," + max + "," + perfect + "," + great + "," + good + "," + bad + "," + miss + ")");
+        }
+    }
+}
