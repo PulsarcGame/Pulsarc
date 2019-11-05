1bf9b766628cc57d9df651a018e296ea850635bf
FRUP <flyingrabidunicornpig@gmail.com>
2019-11-04 21:36:39 -0800

    quickfix Crosshair not being in the right location

--- Pulsarc/UI/Screens/Gameplay/Crosshair.cs
+++ Pulsarc/UI/Screens/Gameplay/Crosshair.cs
@@ -23,8 +23,8 @@ namespace Pulsarc.UI.Screens.Gameplay
             int width = Pulsarc.CurrentWidth;
             int height = Pulsarc.CurrentHeight;
 
-            origin.X = (width / 2) + (Texture.Width - width / 2);
-            origin.Y = (height / 2) + (Texture.Height - height / 2);
+            origin.X = (width / 2) + ((Texture.Width - width) / 2);
+            origin.Y = (height / 2) + ((Texture.Height - height) / 2);
 
             // Set the diameter and resize
             Resize(baseCrosshairDiameter * Pulsarc.HeightScale);
