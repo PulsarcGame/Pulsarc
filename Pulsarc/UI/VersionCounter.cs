f42ef5e3cfa9a3e6aa72c2cfd839656bda746aea
FRUP <flyingrabidunicornpig@gmail.com>
2020-01-23 14:40:26 -0800

    update Version Counter

--- Pulsarc/UI/VersionCounter.cs
+++ Pulsarc/UI/VersionCounter.cs
@@ -4,17 +4,13 @@ namespace Pulsarc.UI
 {
     public class VersionCounter : TextDisplayElement
     {
-        private const string VERSION_NAME = "v1.2.0-alpha";
-
+        private const string VERSION_NAME = "v1.3.0-alpha";
         
         // Temporary until we fix font shit.
-        private const int X_OFFSET = -20;
+        private const int X_OFFSET = -22;
         private const int Y_OFFSET = -6;
 
         public VersionCounter(Vector2 position, int fontSize = 20, Anchor anchor = Anchor.BottomRight)
-            : base(VERSION_NAME, position, fontSize, anchor)
-        {
-            Move(X_OFFSET, Y_OFFSET);
-        }
+            : base(VERSION_NAME, position, fontSize, anchor) => Move(X_OFFSET, Y_OFFSET);
     }
 }
